95effdf345b0dfda141a464430dbfbc9c0a8a663
Hridoy Sankar Dutta <hridaydutta123@gmail.com>
2018-09-05 16:20:33 +0530

    Added twitter oauth

--- app.py
+++ app.py
@@ -1,36 +1,119 @@
-from flask import Flask, redirect, url_for
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
-from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-
-app.config.from_object('config')
 app.secret_key = "SCoRe"
 blueprint = make_twitter_blueprint(
     api_key="HyFodXHKk8h4wFqWCMVVASVmb",
     api_secret="aEb76Hg3I80DEO6ls03Qq6lY1terCooGPVzQ78YxWJnn2xTJra",
 )
 app.register_blueprint(blueprint, url_prefix="/login")
+#db = SQLAlchemy(app)
 
-@app.route("/")
-def index():
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+
+@app.route('/')
+def home():
     if not twitter.authorized:
         return redirect(url_for("twitter.login"))
     resp = twitter.get("account/settings.json")
-    assert resp.ok
-    # return "You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])
-    # return render_template('pages/placeholder.about.html')
-    # return redirect(url_for('/about'))
-    return render_template('index.html')
+    # assert resp.ok
+    screenname = resp.json()["screen_name"] 
+    return render_template('pages/placeholder.home.html', screenname=screenname)
 
 
 @app.route('/about')
 def about():
     return render_template('pages/placeholder.about.html')
-if __name__ == "__main__":
-    app.run()
+
+
+@app.route('/login')
+def login():
+    form = LoginForm(request.form)
+    return render_template('forms/login.html', form=form)
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm(request.form)
+    return render_template('forms/register.html', form=form)
+
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
+# Error handlers.
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
